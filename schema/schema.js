fc94c6e8f0db8ee657843c335cde5fae53cf7418
Jacky Tang <jackytck@gmail.com>
2017-03-22 23:13:41 +0800

    :sparkles: editUser mutation

--- schema/schema.js
+++ schema/schema.js
@@ -93,6 +93,20 @@ const mutation = new GraphQLObjectType({
           .then(res => res.data)
           .catch(err => console.warn(err.response.status))
       }
+    },
+    editUser: {
+      type: UserType,
+      args: {
+        id: { type: new GraphQLNonNull(GraphQLString) },
+        firstName: { type: GraphQLString },
+        age: { type: GraphQLInt },
+        companyId: { type: GraphQLString }
+      },
+      resolve (parentValue, args) {
+        return axios.patch(`http://localhost:3000/users/${args.id}`, args)
+          .then(res => res.data)
+          .catch(err => console.warn(err.response.status))
+      }
     }
   }
 })
